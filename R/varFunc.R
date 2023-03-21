5cea2f1b2613d723945ffcd256d1733b27bbb22d
benjaminmisiuk <benjaminmisiuk@gmail.com>
2023-03-21 06:26:35 -0300

    fix semivariance fn

--- R/varFunc.R
+++ R/varFunc.R
@@ -6,28 +6,23 @@
 #' Transforms a vector of distances `h` to semivariance according to a semivariogram model function. Note this does not include a nugget component, which should be added to the output of these functions if desired.
 #' 
 #' @param h Vector of distances.
-#' @param a Numerical. Fitted range parameter from a varogram model.
+#' @param a Numerical. Fitted range parameter from a variogram model.
 #' 
 #' @examples
 #' 
 #' @export
 #' 
 
-#exponential function
-Exp <- function(h, a) 1 - exp((-h/a))
-
-#' @rdname varFunc
-#' @export
-#' 
-
-#Gaussian function
-Gau <- function(h, a) 1 - exp(-(h/a)^2)
-
-#spherical function
-Sph <- function(h, a){
-  ifelse(
-    h > a,
-    1,
-    ((3*h) / (2*a)) - ((1/2)*(h/a)^3)
-  )
+varFunc <- function(h, a, fun){
+  if(fun == 'Exp') g <- 1 - exp((-h/a))
+  if(fun == 'Gau') g <- 1 - exp(-(h/a)^2)
+  if(fun == 'Sph'){
+    g <- ifelse(
+      h > a,
+      1,
+      ((3*h) / (2*a)) - ((1/2)*(h/a)^3)
+    )
+  }
+  
+  return(g)
 }
