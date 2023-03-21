be3c71b9e963048e53b6b3e267aee3357c73f76c
benjaminmisiuk <benjaminmisiuk@gmail.com>
2023-03-21 06:58:12 -0300

    upddate varFunc docs

--- R/varFunc.R
+++ R/varFunc.R
@@ -3,12 +3,29 @@
 #' Functions to transform distance into semivariance.
 #' 
 #' @details 
-#' Transforms a vector of distances `h` to semivariance according to a semivariogram model function. Note this does not include a nugget component, which should be added to the output of these functions if desired.
+#' Transforms a vector of distances `h` to semivariance $\gamma(h)$ according to a semivariogram model function. Note this does not include nugget or sill components
+#' Use $c(0)+c(varFunc())$, for example, to plot semivariance, where $c(0)$ is the nugget and $c$ is the partial sill.
+#' Currently supported model functions are the exponential, Gaussian, and spherical.
 #' 
 #' @param h Vector of distances.
 #' @param a Numerical. Fitted range parameter from a variogram model.
+#' @param fun Character string indicating the variogram model function. Must be one of "Exp", "Gau", or "Sph".
+#' 
+#' @return A vector of semivariance values.
 #' 
 #' @examples
+#' #set a range parameter (e.g., taken from gstat::fit.variogram())
+#' r = 250
+#' 
+#' #generate some distances to convert to semivariances
+#' d <- seq(0, 1000, 50)
+#' 
+#' varFunc(h=d, a=r, fun = 'Exp')
+#' 
+#' #look at plots of the different functions
+#' plot(varFunc(h=d, a=r, fun = 'Exp'))
+#' points(varFunc(h=d, a=r, fun = 'Gau'), col = 'red')
+#' points(varFunc(h=d, a=r, fun = 'Sph'), col = 'blue')
 #' 
 #' @export
 #' 
